7a9cef67cfe0ed6fc50dd71f66c879648d8f2675
Damian Bis <dambis994132@gmail.com>
2021-03-22 00:33:14 +0100

    feat_database_test

--- WebApi/WebApiTest/DatabaseTest/MapperTest/MapperTest.cs
+++ WebApi/WebApiTest/DatabaseTest/MapperTest/MapperTest.cs
@@ -5,121 +5,132 @@ using WebApi.Models.DTO;
 using Xunit;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.VisualStudio;
 
 namespace WebApiTest.DatabaseTest.MapperTest
 {
     public class MapperTest
     {
-       
+
+        List<Person> getMockedPersonList()
+        {
+            return new List<Person>
+            {
+                    new Person { PersonID = 1, Address = "Koszykowa 3", City = "Warszawa", FirstName = "Damian", LastName = "Bis" },
+                    new Person { PersonID = 3, Address = "Skaryszewska 12" },
+                    new Person { PersonID = 4, Address = "Koszykowa 3", City = "Warszawa", FirstName = "Damian", LastName = "Cis " },
+                    new Person()
+             };
+        }
+        List<PersonDTO> getMockedPersonDTOList()
+        {
+            return new List<PersonDTO>
+            {
+                    new PersonDTO { PersonID = 1, Address = "Koszykowa 3", City = "Warszawa", FirstName = "Damian", LastName = "Bis" },
+                    new PersonDTO { PersonID = 3, Address = "Skaryszewska 12" },
+                    new PersonDTO { PersonID = 4, Address = "Koszykowa 3", City = "Warszawa", FirstName = "Damian", LastName = "Cis " },
+                    new PersonDTO ()
+             };
+
+        }
+
+
         [Fact]
         public void POCOToDTOMapping1()
         {
-            Person input = new Person { PersonID = 1, Address = "Koszykowa 3", City = "Warszawa", FirstName = "Damian", LastName = "Bis" };
-            PersonDTO expectedResult = new PersonDTO { PersonID = 1, Address = "Koszykowa 3", City = "Warszawa", FirstName = "Damian", LastName = "Bis" };
-
+            Person input = getMockedPersonList()[0];
             PersonDTO result = Mapper.Map(input);
 
 
-            Assert.Equal(result.PersonID, expectedResult.PersonID);
-            Assert.Equal(result.FirstName, expectedResult.FirstName);
-            Assert.Equal(result.LastName, expectedResult.LastName);
-            Assert.Equal(result.Address, expectedResult.Address);
-            Assert.Equal(result.City, expectedResult.City);
+            Assert.Equal(input.PersonID, result.PersonID);
+            Assert.Equal(input.FirstName, result.FirstName);
+            Assert.Equal(input.LastName, result.LastName);
+            Assert.Equal(input.Address, result.Address);
+            Assert.Equal(input.City, result.City);
         }
 
         [Fact]
         public void POCOToDTOMapping2()
         {
-            Person input = new Person { PersonID = 3, Address = "Skaryszewska 12"};
-            PersonDTO expectedResult = new PersonDTO { PersonID = 3, Address = "Skaryszewska 12" };
-
+            Person input = getMockedPersonList()[1];
             PersonDTO result = Mapper.Map(input);
 
 
-            Assert.Equal(result.PersonID, expectedResult.PersonID);
-            Assert.Equal(result.FirstName, expectedResult.FirstName);
-            Assert.Equal(result.LastName, expectedResult.LastName);
-            Assert.Equal(result.Address, expectedResult.Address);
-            Assert.Equal(result.City, expectedResult.City);
+            Assert.Equal(input.PersonID, result.PersonID);
+            Assert.Equal(input.FirstName, result.FirstName);
+            Assert.Equal(input.LastName, result.LastName);
+            Assert.Equal(input.Address, result.Address);
+            Assert.Equal(input.City, result.City);
         }
 
         [Fact]
         public void POCOToDTOMapping3()
         {
-            Person input = new Person ();
-            PersonDTO expectedResult = new PersonDTO ();
-
+            Person input = getMockedPersonList()[2];
             PersonDTO result = Mapper.Map(input);
 
 
-            Assert.Equal(result.PersonID, expectedResult.PersonID);
-            Assert.Equal(result.FirstName, expectedResult.FirstName);
-            Assert.Equal(result.LastName, expectedResult.LastName);
-            Assert.Equal(result.Address, expectedResult.Address);
-            Assert.Equal(result.City, expectedResult.City);
+            Assert.Equal(input.PersonID, result.PersonID);
+            Assert.Equal(input.FirstName, result.FirstName);
+            Assert.Equal(input.LastName, result.LastName);
+            Assert.Equal(input.Address, result.Address);
+            Assert.Equal(input.City, result.City);
         }
 
         [Fact]
         public void POCOToDTOMapping4()
         {
-            Person input = new Person { PersonID = 4, Address = "Koszykowa 3", City = "Warszawa", FirstName = "Damian", LastName = " Bis" };
-            PersonDTO expectedResult = new PersonDTO { PersonID = 1, Address = "Koszykowa 3", City = "Warszawa", FirstName = "Damian", LastName = "Bis" };
-
+            Person input = getMockedPersonList()[3];
             PersonDTO result = Mapper.Map(input);
 
 
-            Assert.NotEqual(result.PersonID, expectedResult.PersonID);
-            Assert.Equal(result.FirstName, expectedResult.FirstName);
-            Assert.NotEqual(result.LastName, expectedResult.LastName);
-            Assert.Equal(result.Address, expectedResult.Address);
-            Assert.Equal(result.City, expectedResult.City);
+            Assert.Equal(input.PersonID, result.PersonID);
+            Assert.Equal(input.FirstName, result.FirstName);
+            Assert.Equal(input.LastName, result.LastName);
+            Assert.Equal(input.Address, result.Address);
+            Assert.Equal(input.City, result.City);
         }
 
         [Fact]
         public void DTOToPOCOMapping1()
         {
-            PersonDTO input = new PersonDTO { PersonID = 1, Address = "Koszykowa 3", City = "Warszawa", FirstName = "Damian", LastName = "Bis" };
-            Person expectedResult = new Person { PersonID = 1, Address = "Koszykowa 3", City = "Warszawa", FirstName = "Damian", LastName = "Bis" };
-
+            PersonDTO input = getMockedPersonDTOList()[0];
             Person result = Mapper.Map(input);
 
 
-            Assert.Equal(result.PersonID, expectedResult.PersonID);
-            Assert.Equal(result.FirstName, expectedResult.FirstName);
-            Assert.Equal(result.LastName, expectedResult.LastName);
-            Assert.Equal(result.Address, expectedResult.Address);
-            Assert.Equal(result.City, expectedResult.City);
+            Assert.Equal(input.PersonID, result.PersonID);
+            Assert.Equal(input.FirstName, result.FirstName);
+            Assert.Equal(input.LastName, result.LastName);
+            Assert.Equal(input.Address, result.Address);
+            Assert.Equal(input.City, result.City);
         }
 
         [Fact]
         public void DTOToPOCOMapping2()
         {
-            PersonDTO input = new PersonDTO { PersonID = 1, Address = "Koszykowa 3", City = "Warszawa", FirstName = "Damian", LastName = "Bis" };
-            Person expectedResult = new Person { PersonID = 1, Address = "Koszykowa 3", City = "Warszawa", FirstName = "Damian", LastName = "Bis" };
-
+            PersonDTO input = getMockedPersonDTOList()[1];
             Person result = Mapper.Map(input);
 
 
-            Assert.Equal(result.PersonID, expectedResult.PersonID);
-            Assert.Equal(result.FirstName, expectedResult.FirstName);
-            Assert.Equal(result.LastName, expectedResult.LastName);
-            Assert.Equal(result.Address, expectedResult.Address);
-            Assert.Equal(result.City, expectedResult.City);
+            Assert.Equal(input.PersonID, result.PersonID);
+            Assert.Equal(input.FirstName, result.FirstName);
+            Assert.Equal(input.LastName, result.LastName);
+            Assert.Equal(input.Address, result.Address);
+            Assert.Equal(input.City, result.City);
         }
 
         [Fact]
         public void DTOToPOCOMapping3()
         {
-            PersonDTO input = new PersonDTO { PersonID = 1, Address = "Koszykowa 3", City = "Warszawa", FirstName = "Damian", LastName = "Bis" };
-            Person expectedResult = new Person { PersonID = 1, Address = "Koszykowa 3", City = "Warszawa", FirstName = "Damian", LastName = "Bis" };
-
+            PersonDTO input = getMockedPersonDTOList()[2];
             Person result = Mapper.Map(input);
 
-            Assert.Equal(result.PersonID, expectedResult.PersonID);
-            Assert.Equal(result.FirstName, expectedResult.FirstName);
-            Assert.Equal(result.LastName, expectedResult.LastName);
-            Assert.Equal(result.Address, expectedResult.Address);
-            Assert.Equal(result.City, expectedResult.City);
+
+            Assert.Equal(input.PersonID, result.PersonID);
+            Assert.Equal(input.FirstName, result.FirstName);
+            Assert.Equal(input.LastName, result.LastName);
+            Assert.Equal(input.Address, result.Address);
+            Assert.Equal(input.City, result.City);
         }
 
 
@@ -127,17 +138,15 @@ namespace WebApiTest.DatabaseTest.MapperTest
         [Fact]
         public void DTOToPOCOMapping4()
         {
-            Person input = new Person { PersonID = 1, Address = "Koszykowa 3", City = "Warszawa", FirstName = "Dam ian", LastName = "Bis" };
-            PersonDTO expectedResult = new PersonDTO { PersonID = 1, Address = "Koszykowa 3", City = "Warssszawa", FirstName = "Damian", LastName = "Bis" };
-
-            PersonDTO result = Mapper.Map(input);
+            PersonDTO input = getMockedPersonDTOList()[3];
+            Person result = Mapper.Map(input);
 
 
-            Assert.Equal(result.PersonID, expectedResult.PersonID);
-            Assert.NotEqual(result.FirstName, expectedResult.FirstName);
-            Assert.Equal(result.LastName, expectedResult.LastName);
-            Assert.Equal(result.Address, expectedResult.Address);
-            Assert.NotEqual(result.City, expectedResult.City);
+            Assert.Equal(input.PersonID, result.PersonID);
+            Assert.Equal(input.FirstName, result.FirstName);
+            Assert.Equal(input.LastName, result.LastName);
+            Assert.Equal(input.Address, result.Address);
+            Assert.Equal(input.City, result.City);
         }
 
 
@@ -145,62 +154,22 @@ namespace WebApiTest.DatabaseTest.MapperTest
         [Fact]
         public void POCOToDTOMapping1_List()
         {
-            List<Person> input = new List<Person>
-                {
-                    new Person { PersonID = 1, Address = "Koszykowa 3", City = "Warszawa", FirstName = "Damian", LastName = "Bis" },
-                    new Person { PersonID = 3, Address = "Koszykowa 8", City = "Warszawa", FirstName = "Daniel", LastName = "Dis" },
-                    new Person { PersonID = 8, Address = " Koszykowa 123  ", City = "Warszawa", FirstName = "Dawid", LastName = "Cis" },
-                };
-            List<PersonDTO> expectedResult =new List<PersonDTO>
-                {
-                    new PersonDTO { PersonID = 1, Address = "Koszykowa 3", City = "Warszawa", FirstName = "Damian", LastName = "Bis" },
-                    new PersonDTO { PersonID = 3, Address = "Koszykowa 8", City = "Warszawa", FirstName = "Daniel", LastName = "Dis" },
-                    new PersonDTO { PersonID = 8, Address = " Koszykowa 123  ", City = "Warszawa", FirstName = "Dawid", LastName = "Cis" },
-                };
+            var input = getMockedPersonList();
+            var result = getMockedPersonDTOList();
+            var expectedResult = Mapper.Map(input.AsQueryable());
 
-
-
-            List<PersonDTO> result = Mapper.Map(input.AsQueryable()).ToList();
-
-            for (int i=0; i<input.Count; i++)
-            {
-                Assert.Equal(result[i].PersonID, expectedResult[i].PersonID);
-                Assert.Equal(result[i].FirstName, expectedResult[i].FirstName);
-                Assert.Equal(result[i].LastName, expectedResult[i].LastName);
-                Assert.Equal(result[i].Address, expectedResult[i].Address);
-                Assert.Equal(result[i].City, expectedResult[i].City);
-            }
+            Assert.True(result.All(expectedResult => result.Any(isItem => isItem == expectedResult)));
         }
 
 
         [Fact]
         public void DTOToPOCOMapping1_List()
         {
-            List<PersonDTO> input = new List<PersonDTO>
-                {
-                    new PersonDTO { PersonID = 1, Address = "Koszykowa 3", City = "Warszawa", FirstName = "Damian", LastName = "Bis" },
-                    new PersonDTO { PersonID = 3, Address = "Koszykowa 8", City = "Warszawa", FirstName = "Daniel", LastName = "Dis" },
-                    new PersonDTO { PersonID = 8, Address = " Koszykowa 123  ", City = "Warszawa", FirstName = "Dawid", LastName = "Cis" }
-                };
-            List<Person> expectedResult = new List<Person>
-                {
-                    new Person { PersonID = 1, Address = "Koszykowa 3", City = "Warszawa", FirstName = "Damian", LastName = "Bis" },
-                    new Person { PersonID = 3, Address = "Koszykowa 8", City = "Warszawa", FirstName = "Daniel", LastName = "Dis" },
-                    new Person { PersonID = 8, Address = " Koszykowa 123  ", City = "Warszawa", FirstName = "Dawid", LastName = "Cis" }
-                };
+            var input = getMockedPersonDTOList();
+            var result = getMockedPersonList();
+            var expectedResult = Mapper.Map(input.AsQueryable());
 
-
-
-            List<Person> result = Mapper.Map(input.AsQueryable()).ToList();
-
-            for (int i = 0; i < input.Count; i++)
-            {
-                Assert.Equal(result[i].PersonID, expectedResult[i].PersonID);
-                Assert.Equal(result[i].FirstName, expectedResult[i].FirstName);
-                Assert.Equal(result[i].LastName, expectedResult[i].LastName);
-                Assert.Equal(result[i].Address, expectedResult[i].Address);
-                Assert.Equal(result[i].City, expectedResult[i].City);
-            }
+            Assert.True(result.All(expectedResult => result.Any(isItem => isItem == expectedResult)));
         }
     }
 }
