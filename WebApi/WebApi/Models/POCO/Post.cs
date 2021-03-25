90393e7c96d1dc35145c68dbea2e967c2c1f5c3d
Damian Bis <dambis994132@gmail.com>
2021-03-25 21:09:41 +0100

    feat_post_poco_classes

---
+++ WebApi/WebApi/Models/POCO/Post.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Models.POCO
+{
+    public class Post : IPostable
+    {
+        public int UserId { get; set ; }
+        public DateTime Date { get ; set; }
+        public List<int> Likes { get; set; }
+        public IPostableData Content { get; set; }
+    }
+}
