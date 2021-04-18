8d9a0ccc8114693520f5745b8acb1fdd05b2729b
Gawezi <54313558+Gawezi@users.noreply.github.com>
2021-04-18 15:34:05 +0200

    IO-44 Add Category Repository

--- WebApi/WebApi/Database/Repositories/Implementations/CategoryRepository.cs
+++ WebApi/WebApi/Database/Repositories/Implementations/CategoryRepository.cs
@@ -2,10 +2,32 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebApi.Database.Repositories.Interfaces;
+using WebApi.Models.POCO;
+using WebApi.Services;
 
 namespace WebApi.Database.Repositories.Implementations
 {
-    public class CategoryRepository
+    public class CategoryRepository: Repository<Category>, ICategoryRepository
     {
+        public CategoryRepository(DatabaseContext databaseContext) : base(databaseContext) { }
+        public ServiceResult<IQueryable<Category>> GetCategoriesSubscribedByUser(int userID)
+        {
+
+            //Tak to bedzie wygladalo jak zrobimy newslettera
+
+            //var newstletters = dbContext.Newsletters.Where(newstletters => newstletters.UserID = userID);
+            //List<Category> categories = new List<Category>();
+            //foreach(var newsletter in newstletters)
+            //{
+            //    categories.Add(dbContext.Categories.Where(category => category.CategoryID == newsletter.CategoryID));
+            //}
+
+            //return new ServiceResult<IQueryable<Category>>(categories.AsQueryable());
+
+
+            throw new NotImplementedException();
+        }
+
     }
 }
