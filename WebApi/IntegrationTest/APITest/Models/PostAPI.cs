9ff1fa3ae0949851485c70d079d0c0c42653db38
292885 (Wiktor Czepczyński) <01133096@pw.edu.pl>
2021-04-27 17:01:09 +0200

    IO2-33 tsty integracyjne zasobu Post

---
+++ WebApi/IntegrationTest/APITest/Models/PostAPI.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IntegrationTest
+{
+    public class PostAPI
+    {
+        public int PostID { get; set; }
+        public int UserID { get; set; }
+        public int? Category { get; set; }
+        public DateTime? Datetime { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public bool? IsPromoted { get; set; }
+    }
+}
