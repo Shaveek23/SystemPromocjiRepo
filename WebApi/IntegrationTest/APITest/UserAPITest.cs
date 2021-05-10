e8d4edeea06aa5258c420ae779bc5d308e5b25ce
Damian Bis <dambis994132@gmail.com>
2021-05-10 23:43:45 +0200

    IO2-54-user integration tests added

---
+++ WebApi/IntegrationTest/APITest/UserAPITest.cs
@@ -0,0 +1,257 @@
+﻿using IntegrationTest.APITest.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using WebApi.Models.DTO;
+using Xunit;
+
+namespace IntegrationTest.APITest
+{
+    public class UserAPITest
+    {
+
+        HttpClient client = new HttpClient
+        {
+            BaseAddress = new Uri("https://webapi20210317153051.azurewebsites.net/")
+        };
+
+        public UserApi getUser(string userName)
+        {
+            return new UserApi
+            {
+                UserName = userName,
+                UserEmail = "damian@bis.pl",
+                Timestamp = new DateTime(2008, 3, 1, 7, 0, 0),
+                IsAdmin = false,
+                IsEntrepreneur = false,
+                IsVerified = true,
+                IsActive = true
+            };
+        }
+        public LikeApi getLike()
+        {
+            return new LikeApi
+            {
+                like = true
+            };
+        }
+
+        [Fact]
+        public async void User_ValidCall()
+        {
+            //POST
+            var expectedUser = getUser("created user");
+            var UserRequestMessage = ApiTestManager.CreateRequest(HttpMethod.Post, "users", expectedUser);
+            var UserResult = await client.SendAsync(UserRequestMessage);
+            var UserJsonString = await UserResult.Content.ReadAsStringAsync();
+            var UserId = JsonConvert.DeserializeObject<int>(UserJsonString);
+
+            //GET
+            var beforePutGetRequestMessage = ApiTestManager.CreateRequest(HttpMethod.Get, $"users/{UserId}");
+            var beforePutGetResult = await client.SendAsync(beforePutGetRequestMessage);
+            var beforePutJsonString = await beforePutGetResult.Content.ReadAsStringAsync();
+            var beforePutUser = JsonConvert.DeserializeObject<UserDTO>(beforePutJsonString);
+
+            //PUT
+            var editedUser = getUser("edited username");
+            var PutRequestMessage = ApiTestManager.CreateRequest(HttpMethod.Put, $"users/{UserId}", editedUser);
+            var PutResult = await client.SendAsync(PutRequestMessage);
+            var PutJsonString = await PutResult.Content.ReadAsStringAsync();
+            var isEdited = JsonConvert.DeserializeObject<bool>(PutJsonString);
+
+            //GET
+            var afterPutGetRequestMessage = ApiTestManager.CreateRequest(HttpMethod.Get, $"users/{UserId}");
+            var afterPutGetResult = await client.SendAsync(afterPutGetRequestMessage);
+            var afterPutJsonString = await afterPutGetResult.Content.ReadAsStringAsync();
+            var afterPutUser = JsonConvert.DeserializeObject<UserDTO>(afterPutJsonString);
+
+            //DELETE
+            var DeleteRequestMessage = ApiTestManager.CreateRequest(HttpMethod.Delete, $"users/{UserId}");
+            var DeleteResult = await client.SendAsync(DeleteRequestMessage);
+            var DeleteJsonString = await DeleteResult.Content.ReadAsStringAsync();
+            var isDeleted = JsonConvert.DeserializeObject<bool>(DeleteJsonString);
+            //GET
+            var afterDeleteGetRequestMessage = ApiTestManager.CreateRequest(HttpMethod.Get, $"users/{UserId}");
+            var afterDeleteGetResult = await client.SendAsync(afterDeleteGetRequestMessage);
+
+            Assert.Equal(HttpStatusCode.OK, UserResult.StatusCode);
+            Assert.Equal(HttpStatusCode.OK, beforePutGetResult.StatusCode);
+            Assert.Equal(HttpStatusCode.OK, PutResult.StatusCode);
+            Assert.Equal(HttpStatusCode.OK, afterPutGetResult.StatusCode);
+            Assert.Equal(HttpStatusCode.NotFound, afterDeleteGetResult.StatusCode);
+
+            Assert.Equal(UserId, beforePutUser.ID);
+            Assert.Equal(beforePutUser.UserName, expectedUser.UserName);
+            Assert.Equal(afterPutUser.UserName, editedUser.UserName);
+            Assert.True(isEdited);
+            Assert.True(isDeleted);
+
+        }
+        [Fact]
+        public async void AllUsers_ValidCall()
+        {
+            //Getall
+            var beforePostGetRequestMessage = ApiTestManager.CreateRequest(HttpMethod.Get, "users");
+            var beforePostGetResult = await client.SendAsync(beforePostGetRequestMessage);
+            var beforePostJsonString = await beforePostGetResult.Content.ReadAsStringAsync();
+            List<UserDTO> beforePostUsers = JsonConvert.DeserializeObject<List<UserDTO>>(beforePostJsonString);
+
+            //POST
+            var expectedUser = getUser("New user to list");
+            var UserRequestMessage = ApiTestManager.CreateRequest(HttpMethod.Post, "users", expectedUser);
+            var UserResult = await client.SendAsync(UserRequestMessage);
+            var UserJsonString = await UserResult.Content.ReadAsStringAsync();
+            var UserId = JsonConvert.DeserializeObject<int>(UserJsonString);
+
+            //Getall
+
+            var afterPostGetRequestMessage = ApiTestManager.CreateRequest(HttpMethod.Get, "users");
+            var afterPostGetResult = await client.SendAsync(afterPostGetRequestMessage);
+            var afterPostJsonString = await afterPostGetResult.Content.ReadAsStringAsync();
+            List<UserDTO> afterPostUsers = JsonConvert.DeserializeObject<List<UserDTO>>(afterPostJsonString);
+
+            //PUT
+            var editedUser = getUser("edited");
+            var PutRequestMessage = ApiTestManager.CreateRequest(HttpMethod.Put, $"users/{UserId}", editedUser);
+            var PutResult = await client.SendAsync(PutRequestMessage);
+            var PutJsonString = await PutResult.Content.ReadAsStringAsync();
+            var isEdited = JsonConvert.DeserializeObject<bool>(PutJsonString);
+
+            //Getall
+
+            var afterPutGetRequestMessage = ApiTestManager.CreateRequest(HttpMethod.Get, "users");
+            var afterPutGetResult = await client.SendAsync(afterPutGetRequestMessage);
+            var afterPutJsonString = await afterPutGetResult.Content.ReadAsStringAsync();
+            List<UserDTO> afterPutUsers = JsonConvert.DeserializeObject<List<UserDTO>>(afterPutJsonString);
+
+            //DELETE
+            var DeleteRequestMessage = ApiTestManager.CreateRequest(HttpMethod.Delete, $"users/{UserId}");
+            var DeleteResult = await client.SendAsync(DeleteRequestMessage);
+            var DeleteJsonString = await DeleteResult.Content.ReadAsStringAsync();
+            var isDeleted = JsonConvert.DeserializeObject<bool>(DeleteJsonString);
+
+            //Getall
+
+            var afterDeleteGetRequestMessage = ApiTestManager.CreateRequest(HttpMethod.Get, "users");
+            var afterDeleteGetResult = await client.SendAsync(afterDeleteGetRequestMessage);
+            var afterDeleteJsonString = await afterDeleteGetResult.Content.ReadAsStringAsync();
+            List<UserDTO> afterDeleteUsers = JsonConvert.DeserializeObject<List<UserDTO>>(afterDeleteJsonString);
+
+            Assert.Equal(HttpStatusCode.OK, UserResult.StatusCode);
+            Assert.Equal(HttpStatusCode.OK, afterPutGetResult.StatusCode);
+            Assert.Equal(HttpStatusCode.OK, PutResult.StatusCode);
+            Assert.Equal(HttpStatusCode.OK, afterPutGetResult.StatusCode);
+            Assert.Equal(HttpStatusCode.OK, afterDeleteGetResult.StatusCode);
+
+            Assert.NotEmpty(beforePostUsers);
+            Assert.NotEmpty(afterPostUsers);
+            Assert.NotEmpty(afterPutUsers);
+            Assert.NotEmpty(afterDeleteUsers);
+
+            Assert.Equal(beforePostUsers.Count, afterDeleteUsers.Count);
+            Assert.Equal(afterPostUsers.Count, afterPutUsers.Count);
+            Assert.Equal(beforePostUsers.Count + 1, afterPostUsers.Count);
+        }
+
+
+        [Fact]
+        public async void PostUser_InvalidCall_NoUserName()
+        {
+            //POST
+            var expectedUser = getUser("SomeUsername");
+            expectedUser.UserName = null;
+            var UserRequestMessage = ApiTestManager.CreateRequest(HttpMethod.Post, "users", expectedUser);
+            var UserResult = await client.SendAsync(UserRequestMessage);
+
+            Assert.Equal(HttpStatusCode.BadRequest, UserResult.StatusCode);
+        }
+
+        [Fact]
+        public async void PostUser_InvalidCall_NoUserEmail()
+        {
+            //POST
+            var expectedUser = getUser("SomeUsername");
+            expectedUser.UserEmail = null;
+            var UserRequestMessage = ApiTestManager.CreateRequest(HttpMethod.Post, "users", expectedUser);
+            var UserResult = await client.SendAsync(UserRequestMessage);
+
+            Assert.Equal(HttpStatusCode.BadRequest, UserResult.StatusCode);
+        }
+
+        [Fact]
+        public async void PostUser_InvalidCall_No_IsVerified_Information()
+        {
+            //POST
+            var expectedUser = getUser("SomeUsername");
+            expectedUser.IsVerified = null;
+            var UserRequestMessage = ApiTestManager.CreateRequest(HttpMethod.Post, "users", expectedUser);
+            var UserResult = await client.SendAsync(UserRequestMessage);
+
+            Assert.Equal(HttpStatusCode.BadRequest, UserResult.StatusCode);
+        }
+
+        [Fact]
+        public async void PostUser_InvalidCall_No_IsActive_Information()
+        {
+            //POST
+            var expectedUser = getUser("SomeUsername");
+            expectedUser.IsActive = null;
+            var UserRequestMessage = ApiTestManager.CreateRequest(HttpMethod.Post, "users", expectedUser);
+            var UserResult = await client.SendAsync(UserRequestMessage);
+
+            Assert.Equal(HttpStatusCode.BadRequest, UserResult.StatusCode);
+        }
+
+        [Fact]
+        public async void PostUser_InvalidCall_No_IsAdmin_Information()
+        {
+            //POST
+            var expectedUser = getUser("SomeUsername");
+            expectedUser.IsAdmin = null;
+            var UserRequestMessage = ApiTestManager.CreateRequest(HttpMethod.Post, "users", expectedUser);
+            var UserResult = await client.SendAsync(UserRequestMessage);
+
+            Assert.Equal(HttpStatusCode.BadRequest, UserResult.StatusCode);
+        }
+
+        [Fact]
+        public async void PostUser_InvalidCall_No_IsEntrepreneur_Information()
+        {
+            //POST
+            var expectedUser = getUser("SomeUsername");
+            expectedUser.IsEntrepreneur = null;
+            var UserRequestMessage = ApiTestManager.CreateRequest(HttpMethod.Post, "users", expectedUser);
+            var UserResult = await client.SendAsync(UserRequestMessage);
+
+            Assert.Equal(HttpStatusCode.BadRequest, UserResult.StatusCode);
+        }
+
+        [Fact]
+        public async void PostUser_InvalidCall_No_TimeStamp_Information()
+        {
+            //POST
+            var expectedUser = getUser("SomeUsername");
+            expectedUser.Timestamp = null;
+            var UserRequestMessage = ApiTestManager.CreateRequest(HttpMethod.Post, "users", expectedUser);
+            var UserResult = await client.SendAsync(UserRequestMessage);
+
+            Assert.Equal(HttpStatusCode.BadRequest, UserResult.StatusCode);
+        }
+
+
+        [Fact]
+        public async void PostUser_InvalidCall_NoUserIdHeader()
+        {
+            //POST
+            var expectedUser = getUser("SomeUsername");
+            var UserRequestMessage = ApiTestManager.CreateRequest(HttpMethod.Post, "users", expectedUser);
+            UserRequestMessage.Headers.Clear();
+            var UserResult = await client.SendAsync(UserRequestMessage);
+
+            Assert.Equal(HttpStatusCode.BadRequest, UserResult.StatusCode);
+        }
+    }
+}
---
+++ WebApi/IntegrationTest/IntegrationTest/UserIntegrationTest.cs
@@ -0,0 +1,184 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WebApi.Controllers;
+using WebApi.Database;
+using WebApi.Database.Repositories.Implementations;
+using WebApi.Models.DTO;
+using WebApi.Models.POCO;
+using WebApi.Services.Serives_Implementations;
+using Xunit;
+
+namespace IntegrationTest.IntegrationTest
+{
+    public class UserIntegrationTest
+    {
+        public UserController GetUserController(DbContextOptions<DatabaseContext> options)
+        {
+            var logger = new Mock<ILogger<UserController>>();
+            var databaseContext = new DatabaseContext(options);
+            var UserRepository = new UserRepository(databaseContext);
+            var UserService = new UserService(UserRepository);
+            var UserController = new UserController(logger.Object, UserService);
+            return UserController;
+        }
+
+
+
+        [Fact]
+        public void GetUser_ValidCall()
+        {
+            var options = new DbContextOptionsBuilder<DatabaseContext>()
+               .UseInMemoryDatabase(databaseName: "GetUser_ValidCall").Options;
+            var expectedUser = new User() { UserID = 1, UserName = "userName", UserEmail = "test@eamil.com", Active = true, IsAdmin = false, IsEnterprenuer = false, IsVerified = false, Timestamp = DateTime.Now };
+
+            using (var dbContext = new DatabaseContext(options))
+            {
+                dbContext.Add(expectedUser);
+                dbContext.SaveChanges();
+            }
+
+            var userController = GetUserController(options);
+
+            var actual = (UserDTO)((ObjectResult)userController.Get(expectedUser.UserID).Result).Value;
+
+            Assert.NotNull(actual);
+            Assert.Equal(expectedUser.UserID, actual.ID);
+            Assert.Equal(expectedUser.UserName, actual.UserName);
+            Assert.Equal(expectedUser.UserEmail, actual.UserEmail);
+            Assert.Equal(expectedUser.Timestamp, actual.Timestamp);
+            Assert.Equal(expectedUser.IsAdmin, actual.IsAdmin);
+            Assert.Equal(expectedUser.Active, actual.IsActive);
+            Assert.Equal(expectedUser.IsEnterprenuer, actual.IsEntrepreneur);
+            Assert.Equal(expectedUser.IsVerified, actual.IsVerified);
+        }
+
+        [Fact]
+        public void GetUsers_ValidCall()
+        {
+            var options = new DbContextOptionsBuilder<DatabaseContext>()
+               .UseInMemoryDatabase(databaseName: "GetUsers_ValidCall").Options;
+            var expectedUsers = new List<User>{
+                new User { UserID = 1, UserEmail = "email132@mini.pw.edu.pl", UserName = "student", Timestamp = new DateTime(2021, 4, 16, 8, 4, 12), IsEnterprenuer = false, IsAdmin = false, IsVerified = false },
+                new User { UserID = 2, UserEmail = "321321312@ck.pl", UserName = "student2", Timestamp = new DateTime(2021, 2, 6, 4, 2, 12), IsEnterprenuer = false, IsAdmin = true, IsVerified = false },
+                new User { UserID = 3, UserEmail = "dsadas@adsa.pl", UserName = "student2223", Timestamp = new DateTime(2020, 1, 11, 5, 4, 12), IsEnterprenuer = true, IsAdmin = false, IsVerified = false }
+                };
+
+            using (var dbContext = new DatabaseContext(options))
+            {
+                foreach (var user in expectedUsers)
+                    dbContext.Add(user);
+                dbContext.SaveChanges();
+            }
+
+            var userController = GetUserController(options);
+
+            var actual = (IEnumerable<UserDTO>)((ObjectResult)userController.GetAll().Result).Value;
+            var count = actual.Count();
+            Assert.NotNull(actual);
+            Assert.NotEmpty(actual);
+            Assert.Equal(expectedUsers.Count, count);
+        }
+
+
+
+        public UserDTO GetUserEditDTO()
+        {
+            return new UserDTO() { UserEmail = "email132@mini.pw.edu.pl", UserName = "student", Timestamp = new DateTime(2021, 4, 16, 8, 4, 12), IsEntrepreneur = false, IsAdmin = false, IsVerified = false, IsActive = true };
+        }
+
+        [Fact]
+        public void AddUser_ValidCall()
+        {
+            var options = new DbContextOptionsBuilder<DatabaseContext>()
+               .UseInMemoryDatabase(databaseName: "AddUser_ValidCall").Options;
+
+            var userController = GetUserController(options);
+
+            var userDTO = GetUserEditDTO();
+            int userCreatorID = 1;
+            var result = userController.AddUser(userCreatorID, userDTO).Result.Value;
+
+            var databaseContext = new DatabaseContext(options);
+            var actual = databaseContext.Users.FirstOrDefault();
+
+            Assert.NotNull(actual);
+            Assert.Equal(userDTO.UserName, actual.UserName);
+            Assert.Equal(userDTO.UserEmail, actual.UserEmail);
+            Assert.Equal(userDTO.Timestamp, actual.Timestamp);
+            Assert.Equal(userDTO.IsAdmin, actual.IsAdmin);
+            Assert.Equal(userDTO.IsActive, actual.Active);
+            Assert.Equal(userDTO.IsEntrepreneur, actual.IsEnterprenuer);
+            Assert.Equal(userDTO.IsVerified, actual.IsVerified);
+        }
+
+        [Fact]
+        public void EditUser_ValidCall()
+        {
+            //var options = new DbContextOptionsBuilder<DatabaseContext>()
+            //   .UseInMemoryDatabase(databaseName: "EditUser_ValidCall").Options;
+
+            //var users = new List<User>{
+            //    new User { UserID = 1, UserEmail = "email132@mini.pw.edu.pl", UserName = "student", Timestamp = new DateTime(2021, 4, 16, 8, 4, 12), Active=true, IsEnterprenuer = false, IsAdmin = false, IsVerified = false },
+            //    new User { UserID = 2, UserEmail = "321321312@ck.pl", UserName = "student2", Timestamp = new DateTime(2021, 2, 6, 4, 2, 12), Active=true, IsEnterprenuer = false, IsAdmin = true, IsVerified = false },
+            //    new User { UserID = 3, UserEmail = "dsadas@adsa.pl", UserName = "student2223", Timestamp = new DateTime(2020, 1, 11, 5, 4, 12), Active=true, IsEnterprenuer = true, IsAdmin = false, IsVerified = false }
+            //    };
+            //using (var dbContext = new DatabaseContext(options))
+            //{
+            //    foreach (var newUser in users)
+            //        dbContext.Add(newUser);
+            //    dbContext.SaveChanges();
+            //}
+
+            //var userController = GetUserController(options);
+            //int userEditorID = 1;
+            //int editedUserID = 2;
+            //var userDTO = GetUserEditDTO();
+
+            //var result =  userController.EditUser(userEditorID, userDTO, editedUserID).Result.Value;
+
+            //var databaseContext = new DatabaseContext(options);
+
+            //var user = users.Where(x => x.UserID == editedUserID).FirstOrDefault();
+
+            //Assert.True(result);
+            //Assert.Equal(userDTO.UserName, user.UserName);
+            //Assert.Equal(userDTO.UserEmail, user.UserEmail);
+            //Assert.Equal(userDTO.Timestamp, user.Timestamp);
+            //Assert.Equal(userDTO.IsAdmin, user.IsAdmin);
+            //Assert.Equal(userDTO.IsActive, user.Active);
+            //Assert.Equal(userDTO.IsEntrepreneur, user.IsEnterprenuer);
+            //Assert.Equal(userDTO.IsVerified, user.IsVerified);
+        }
+
+        [Fact]
+        public void DeleteUser_ValidCall()
+        {
+            var options = new DbContextOptionsBuilder<DatabaseContext>()
+               .UseInMemoryDatabase(databaseName: "DeleteUser_ValidCall").Options;
+
+            var user = new User() { UserID = 1, UserEmail = "dsadasdsadasdas@mini.pw.edu.pl", UserName = "sadsa", Timestamp = new DateTime(2011, 4, 16, 8, 4, 12), IsEnterprenuer = false, IsAdmin = false, IsVerified = false, Active = true };
+
+            using (var dbContext = new DatabaseContext(options))
+            {
+                dbContext.Add(user);
+                dbContext.SaveChanges();
+            }
+
+            var userController = GetUserController(options);
+
+            var result = userController.DeleteUser(1,1).Result.Value;
+
+            var databaseContext = new DatabaseContext(options);
+            var actual = databaseContext.Posts.FirstOrDefault();
+
+            Assert.Null(actual);
+        }
+
+    }
+}
